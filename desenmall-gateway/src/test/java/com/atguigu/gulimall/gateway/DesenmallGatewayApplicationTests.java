e6e50a672031736854de6c96f3bf0e576edb6390
yangminglin2016 <504945613@qq.com>
2021-04-11 16:33:19 +0800

    增加网关模块

---
+++ desenmall-gateway/src/test/java/com/atguigu/gulimall/gateway/DesenmallGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.gateway;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenmallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
