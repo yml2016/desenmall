fc21f553f1d204af4883127947b2a24b82e54c75
yangminglin2016 <504945613@qq.com>
2021-05-04 17:43:15 +0800

    增加第三方调用的服务功能相关

--- desenmall-product/src/test/java/com/desen/desenmall/product/DesenmallProductApplicationTests.java
+++ desenmall-product/src/test/java/com/desen/desenmall/product/DesenmallProductApplicationTests.java
@@ -44,8 +44,8 @@ class DesenmallProductApplicationTests {
         /*// yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
         String endpoint = "oss-cn-shenzhen.aliyuncs.com";
        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5t99RuZJ7wXaQ1axZq8Q";
-        String accessKeySecret = "6lCHST0wWz3vZaiGcUG6SZxS1Ose3R";
+        String accessKeyId = "LTAI5t99RuZJ71wXaQ1axZq8Q";
+        String accessKeySecret = "6lCHST0wWz3vZaiGcUG64SZxS1Ose3R";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
