44ba873cfe710a57cb2c06f384647e29f5ed1a0f
yangminglin2016 <504945613@qq.com>
2021-10-13 08:15:46 +0800

    增加控制台vue前端项目，已经笔记等

--- desenmall-product/src/main/java/com/desen/desenmall/product/DesenmallProductApplication.java
+++ desenmall-product/src/main/java/com/desen/desenmall/product/DesenmallProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 
@@ -18,7 +19,12 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 public class DesenmallProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesenmallProductApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(DesenmallProductApplication.class, args);
+        /*String[] names = applicationContext.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println("name = " + name);
+        }*/
+
     }
 
 }
